4a305ab829d34eba67afba8ec09549b95fb23907
JamesMarcil <JamesMarcil@users.noreply.github.com>
2016-03-09 19:24:46 -0500

    Fix compiler error in ht_renderer_singleton.

--- source/ht_renderer_singleton.cpp
+++ source/ht_renderer_singleton.cpp
@@ -13,6 +13,7 @@
 **/
 
 #include <ht_renderer_singleton.h>
+#include <ht_time_singleton.H>
 #include <ht_debug.h>
 #ifdef HT_SYS_WINDOWS
 #ifdef DX11_SUPPORT
@@ -127,7 +128,7 @@ namespace Hatchit {
         {
             Renderer& _instance = Renderer::instance();
 
-            _instance.m_renderer->VRender();
+            _instance.m_renderer->VRender(Time::DeltaTime());
         }
 
         void Renderer::Present()

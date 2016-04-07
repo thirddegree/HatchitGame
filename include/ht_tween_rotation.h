fc4b809ddfaee05128c8357c9eb00794d4c8bc7e
Richard Selneck <raselneck@gmail.com>
2016-04-07 14:15:46 -0400

    Add TweenRotation and TweenScale

---
+++ include/ht_tween_rotation.h
@@ -0,0 +1,57 @@
+#include <ht_tween_component.h>
+
+namespace Hatchit {
+
+    namespace Game {
+
+        /**
+         * \brief Defines a tween component that can be used to tween an object's rotation.
+         */
+        class TweenRotation : public TweenComponent
+        {
+        public:
+            /**
+             * \brief Creates a new tween rotation component.
+             */
+            TweenRotation();
+
+            /**
+             * \brief Destroys this tween rotation component.
+             */
+            ~TweenRotation();
+
+            /**
+             * \brief Gets the start tween rotation.
+             */
+            Math::Float3 GetStartRotation() const;
+
+            /**
+             * \brief Gets the end tween rotation.
+             */
+            Math::Float3 GetEndRotation() const;
+
+            /**
+             * \brief Sets the start tween rotation.
+             *
+             * \param value The new start rotation.
+             */
+            void SetStartRotation(const Math::Float3& value);
+
+            /**
+             * \brief Sets the end tween rotation.
+             *
+             * \param value The new end rotation.
+             */
+            void SetEndRotation(const Math::Float3& value);
+
+            /**
+             * \brief Called once per frame while the GameObject is enabled.
+             *
+             * Updates all components first, then all child gameobjects.
+             */
+            void VOnUpdate() final override;
+        };
+
+    }
+
+}
---
+++ source/ht_tween_scale.cpp
@@ -0,0 +1,78 @@
+#include <ht_tween_scale.h>
+#include <ht_gameobject.h>
+
+namespace Hatchit {
+
+    namespace Game {
+
+        /**
+         * \brief Creates a new tween scale component.
+         */
+        TweenScale::TweenScale()
+        {
+            Math::Float3 empty;
+
+            SetStartScale(empty);
+            SetEndScale(empty);
+        }
+
+        /**
+         * \brief Destroys this tween scale component.
+         */
+        TweenScale::~TweenScale()
+        {
+        }
+
+        /**
+         * \brief Gets the start tween scale.
+         */
+        Math::Float3 TweenScale::GetStartScale() const
+        {
+            return m_startValue.GetFloat3();
+        }
+
+        /**
+         * \brief Gets the end tween scale.
+         */
+        Math::Float3 TweenScale::GetEndScale() const
+        {
+            return m_endValue.GetFloat3();
+        }
+
+        /**
+         * \brief Sets the start tween scale.
+         *
+         * \param value The new start scale.
+         */
+        void TweenScale::SetStartScale(const Math::Float3& value)
+        {
+            m_startValue.SetValue(value);
+        }
+
+        /**
+         * \brief Sets the end tween scale.
+         *
+         * \param value The new end scale.
+         */
+        void TweenScale::SetEndScale(const Math::Float3& value)
+        {
+            m_endValue.SetValue(value);
+        }
+
+        /**
+         * \brief Called once per frame while the GameObject is enabled.
+         *
+         * Updates all components first, then all child gameobjects.
+         */
+        void TweenScale::VOnUpdate()
+        {
+            TweenComponent::VOnUpdate();
+
+            Math::Float3 target = m_targetValue.GetFloat3();
+            Math::Vector3 scale(target.m_data);
+            m_owner->GetTransform().SetScale(scale);
+        }
+
+    }
+
+}

a9cb536093395ac32a9a9286694673219e3375f1
tcreeds <tcreeds@gmail.com>
2016-04-12 11:27:06 -0400

    ComponentFactory also checks for components that inherit indirectly from Component.

--- source/ht_component_factory.cpp
+++ source/ht_component_factory.cpp
@@ -6,26 +6,52 @@ import cog
 from os import listdir
 from os.path import isfile, join
 
-files = [f for f in listdir('../../../../HatchitGame/include/') if isfile(join('../../../../HatchitGame/include/', f))]
+class Node():
+    def __init__(self, filename, className, superName):
+        self.filename = filename
+        self.className = className
+        self.superName = superName
+        self.children = []
 
-ComponentFiles = []
+def findChildren(classes, componentList, components, superName):
+    for cl in classes:
+        if cl.superName == superName:
+            components.append(Node(cl.filename, cl.className, cl.superName))
+            componentList.append(cl)
+
+classes = []
+componentList = []
+components = []
+
+files = [f for f in listdir('../../../HatchitGame/include/') if isfile(join('../../../HatchitGame/include/', f))]
 
 for filename in files:
-    with open("../../../../HatchitGame/include/" + filename, 'r') as f:
+    with open("../../../HatchitGame/include/" + filename, 'r') as f:
         for line in f:
-            if ": public Component" in line or ": Component" in line:
-                words = line.strip().split(' ')
-                ComponentFiles.append(filename)
+            if "class" in line and " : " in line and not ("enum" in line):
+                words = line.strip().split(" : ")
+                className = words[0].strip().split(' ')[-1]
+                supers = words[1].strip().split(' ')
+                for superName in supers:
+                    if not (superName == "public") and not (superName == ","):
+                        classes.append(Node(filename, className, superName))
 
+findChildren(classes, componentList, components, "Component")
+while len(componentList) > 0:
+    n = componentList.pop()
+    findChildren(classes, componentList, components, n.className)
 
-for t in ComponentFiles:
-    cog.outl("#include <%s>" % t);
+for t in components:
+    cog.outl("#include <%s>" % t.filename);
 
 ]]]*/
 #include <ht_camera_component.h>
 #include <ht_meshrenderer_component.h>
 #include <ht_test_component.h>
 #include <ht_tween_component.h>
+#include <ht_tween_position.h>
+#include <ht_tween_rotation.h>
+#include <ht_tween_scale.h>
 //[[[end]]]
 
 namespace Hatchit {
@@ -33,32 +59,59 @@ namespace Hatchit {
         Component* ComponentFactory::MakeComponent(std::string type)
         {
             /*[[[cog
+                
                 import cog
                 from os import listdir
                 from os.path import isfile, join
 
-                files = [f for f in listdir('../../../../HatchitGame/include/') if isfile(join('../../../../HatchitGame/include/', f))]
+                files = [f for f in listdir('../../../HatchitGame/include/') if isfile(join('../../../HatchitGame/include/', f))]
                 
-                ComponentTypes = []
-                
+                class Node():
+                    def __init__(self, filename, className, superName):
+                        self.filename = filename
+                        self.className = className
+                        self.superName = superName
+                        self.children = []
+                    def addChild(self, child):
+                        self.children.append(child)
+
+                def findChildren(classes, componentList, components, superName):
+                    for cl in classes:
+                        if cl.superName == superName:
+                            componentList.append(cl)
+                            components.append(Node(cl.filename, cl.className, cl.superName))
+
+                classes = []
+                componentList = []
+                components = []
+
                 for filename in files:
-                    with open("../../../../HatchitGame/include/" + filename, 'r') as f:
+                    with open("../../../HatchitGame/include/" + filename, 'r') as f:
                         for line in f:
-                            if ": public Component" in line or ": Component" in line:
-                                words = line.strip().split(' ')
-                                if words[1] == "HT_API":
-                                    ComponentTypes.append(words[2])
-                                else:
-                                    ComponentTypes.append(words[1])
+                            if "class" in line and " : " in line and not ("enum" in line):
+                                words = line.strip().split(" : ")
+                                className = words[0].strip().split(' ')[-1]
+                                supers = words[1].strip().split(' ')
+                                for superName in supers:
+                                    if not (superName == "public") and not (superName == ","):
+                                        classes.append(Node(filename, className, superName))
+
+                findChildren(classes, componentList, components, "Component")
+                while len(componentList) > 0:
+                    n = componentList.pop()
+                    findChildren(classes, componentList, components, n.className)
                 
-                for t in ComponentTypes:
-                    cog.outl("""if (type == "%s") return new %s();""" % (t, t));
-            
+                for t in components:
+                    cog.outl("""if (type == "%s") return new %s();""" % (t.className, t.className));
+                
             ]]]*/
             if (type == "Camera") return new Camera();
             if (type == "MeshRenderer") return new MeshRenderer();
             if (type == "TestComponent") return new TestComponent();
             if (type == "TweenComponent") return new TweenComponent();
+            if (type == "TweenPosition") return new TweenPosition();
+            if (type == "TweenRotation") return new TweenRotation();
+            if (type == "TweenScale") return new TweenScale();
             //[[[end]]]
             return nullptr;
         }
